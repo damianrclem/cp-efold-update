25c5d5414a864cea5679a357413b07a22ae0794d
Jared Brown <brown.jared25@gmail.com>
2021-06-25 14:16:40 -0400

    Get loan document by title and borrower name

---
+++ __tests__/unit/logic/getLoanDocumentByTitleAndBorrower.unit.test.ts
@@ -0,0 +1,30 @@
+import { getLoanDocumentByTitleAndBorrowerName } from '../../../src/logic/getLoanDocumentByTitleAndBorrowerName';
+
+describe('getLoanDocumentByTitleAndBorrowerName', () => {
+    const loanDocuments = [{
+        title: 'hello',
+        application: {
+            entityName: 'Bert McDonald'
+        }
+    }]
+
+    test('it returns the loan document if the title and borrower name match', () => {
+        const result = getLoanDocumentByTitleAndBorrowerName(loanDocuments, 'hello', 'Bert McDonald');
+        expect(result).toEqual(loanDocuments[0]);
+    });
+
+    test('it returns undefined if the title matches and borrower name does not match', () => {
+        const result = getLoanDocumentByTitleAndBorrowerName(loanDocuments, 'goodbye', 'Bert McDoland');
+        expect(result).toEqual(undefined);
+    });
+
+    test('it returns undefined if the title does not match and the borrower name does', () => {
+        const result = getLoanDocumentByTitleAndBorrowerName(loanDocuments, 'hello', 'Jeanie Black');
+        expect(result).toEqual(undefined);
+    });
+
+    test('it returns undefined if both the title and borrower name does not match', () => {
+        const result = getLoanDocumentByTitleAndBorrowerName(loanDocuments, 'Microsoft Sucks', 'Satya Nadella');
+        expect(result).toEqual(undefined);
+    });
+})
