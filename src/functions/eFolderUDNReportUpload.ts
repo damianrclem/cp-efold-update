1f52dcad25ea1a654b8d877413cc56f42b631b56
Jared Brown <brown.jared25@gmail.com>
2021-06-25 16:13:01 -0400

    Implementation for uploading UDN report

--- src/functions/eFolderUDNReportUpload.ts
+++ src/functions/eFolderUDNReportUpload.ts
@@ -1,6 +1,11 @@
 import { LoggerError } from '@revolutionmortgage/rm-logger';
 import { EventBridgeHandler, EventBridgeEvent } from 'aws-lambda';
 import get from 'lodash/get';
+import { createLoanDocument, getLoan, getLoanDocuments } from '../clients/encompass';
+import { UDN_REPORTS_E_FOLDER_DOCUMENT_TITLE } from '../constants';
+import { getEncompassLoanBorrowerBySocialSecurityNumber } from '../logic/getEncompassLoanBorrowerBySocialSecurityNumber';
+import { getLoanDocumentByTitleAndBorrowerName } from '../logic/getLoanDocumentByTitleAndBorrowerName';
+import { uploadUDNReportToEFolder } from '../logic/uploadUDNReportToEFolder';
 
 export class InvalidParamsError extends LoggerError {
     constructor(message: string, data?: any) {
@@ -24,8 +29,8 @@ type Handler = EventBridgeHandler<EVENT_TYPE, Detail, void>;
 type Event = EventBridgeEvent<EVENT_TYPE, Detail>;
 
 /**
- * An example Event Bridge Lambda
- * @param {Event} event
+ * This lambda will upload a UDN report to an eFolder in Encompass
+ * @param {Event} event - The event that triggers this lambda
  * @returns {Promise<void>}
  */
 export const handler: Handler = async (event: Event): Promise<void> => {
@@ -39,8 +44,20 @@ export const handler: Handler = async (event: Event): Promise<void> => {
         throw new InvalidParamsError("SocialSecurityNumber missing on request payload");
     }
 
-    const pdf = get(event, 'detail.responsePayload.pdf');
+    const pdf = get(event, 'detail.responsePayload.detail.pdf');
     if (!pdf) {
         throw new InvalidParamsError("pdf missing on response payload", event);
     }
+
+    const loanResponse = await getLoan(loanId);
+    const borrower = getEncompassLoanBorrowerBySocialSecurityNumber(socialSecurityNumber, loanResponse.data);
+
+    const loanDocumentsResponse = await getLoanDocuments(loanId);
+    const existingLoanDocument = getLoanDocumentByTitleAndBorrowerName(loanDocumentsResponse.data, UDN_REPORTS_E_FOLDER_DOCUMENT_TITLE, borrower.fullName);
+    if (existingLoanDocument) {
+        await uploadUDNReportToEFolder(loanId, existingLoanDocument.id, pdf);
+    }
+
+    const newLoanDocumentRepsonse = await createLoanDocument(loanId, borrower.applicationId);
+    await uploadUDNReportToEFolder(loanId, newLoanDocumentRepsonse.data.id, pdf);
 };
