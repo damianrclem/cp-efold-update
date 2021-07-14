c07d66249c10d4d77e532ba9c178ed171543d5d8
Jared Brown <brown.jared25@gmail.com>
2021-07-14 08:14:24 -0400

    console

--- src/functions/eFolderUDNReportUpload.ts
+++ src/functions/eFolderUDNReportUpload.ts
@@ -40,6 +40,8 @@ type Event = EventBridgeEvent<EVENT_TYPE, Detail>;
  * @returns {Promise<void>}
  */
 export const handler: Handler = async (event: Event): Promise<void> => {
+    try {
+        
     const loanId = get(event, 'detail.requestPayload.detail.LoanId');
     if (!loanId) {
         throw new InvalidParamsError("LoanId missing on request payload", event);
@@ -78,4 +80,9 @@ export const handler: Handler = async (event: Event): Promise<void> => {
     }
 
     await uploadUDNReportToEFolder(loanId, newLoanDocument.id, pdf);
+    } catch (error) {
+        console.error(error);
+
+        throw error;
+    }
 };
