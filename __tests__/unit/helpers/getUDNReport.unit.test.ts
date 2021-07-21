1892dd8dc6d530ac8ed8d36de91ae2d3991fa115
Jared Brown <brown.jared25@gmail.com>
2021-07-21 16:01:26 -0400

    Test for getting the pdf report from an order

--- __tests__/unit/helpers/getUDNReport.unit.test.ts
+++ __tests__/unit/helpers/getUDNReport.unit.test.ts
@@ -19,4 +19,19 @@ describe('getUDNReport', () => {
             socialSecurityNumber: '123?'
         })).rejects.toThrowError(PDFReportNotFoundError);
     });
+
+    test('returns the pdf report of the UDN order', async () => {
+        getUDNOrder.mockResolvedValue({
+            data: '<EmbeddedContentXML>I am a pdf</EmbeddedContentXML>'
+        });
+
+        const report = await getUDNReport({
+            firstName: 'Bertty',
+            lastName: 'Bert Bert',
+            vendorOrderIdentifier: '123',
+            socialSecurityNumber: '123?'
+        });
+
+        expect(report).toEqual('I am a pdf');
+    });
 });
