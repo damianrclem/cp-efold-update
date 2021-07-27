b2ef6a3c05f6bcc7a2271c558561dc412a200b20
Jared Brown <brown.jared25@gmail.com>
2021-07-27 15:33:37 -0400

    Unit test for saveEncompassLoan handler

--- __tests__/unit/functions/saveEncompassLoan.unit.test.ts
+++ __tests__/unit/functions/saveEncompassLoan.unit.test.ts
@@ -2,6 +2,15 @@
 
 import { InvalidEventParamsError } from "../../../src/common/errors"
 import { handler } from "../../../src/functions/saveEncompassLoan"
+import { mapLoanEventFieldsToDatabaseFields } from "../../../src/helpers/mapLoanEventFieldsToDatabaseFields"
+
+jest.mock('../../../src/helpers/mapLoanEventFieldsToDatabaseFields', () => ({
+    mapLoanEventFieldsToDatabaseFields: jest.fn(),
+}))
+
+jest.mock('../../../src/common/database', () => ({
+    putItem: jest.fn()
+}))
 
 describe('saveEncompassLoan', () => {
     test('throws an InvalidEventParamsError if loan.id is missing on event detail', async () => {
@@ -55,5 +64,24 @@ describe('saveEncompassLoan', () => {
 
         await expect(invalidHandler).rejects.toThrow(InvalidEventParamsError);
         await expect(invalidHandler).rejects.toThrow("Required parameter detail.loan.fields['65'] is missing on event payload")
+    })
+
+    test("it does not blow up", async () => {
+        mapLoanEventFieldsToDatabaseFields.mockReturnValue({
+            Id: 'foo',
+        })
+
+        await expect(handler({
+            detail: {
+                loan: {
+                    id: '1',
+                    fields: {
+                        '4000': 'bert',
+                        '4002': 'mcmuffin',
+                        '65': 'hfhf',
+                    }
+                },
+            }
+        }, {}, () => {})).resolves.not.toThrowError();
     })
 })
