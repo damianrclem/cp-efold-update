77f1ae7a32cf0c2b25af987968ca4761a2b4b930
Jared Brown <brown.jared25@gmail.com>
2021-07-27 15:35:17 -0400

    saveEncompassLoan integration test

---
+++ __tests__/int/functions/saveEncompassLoan.int.test.ts
@@ -0,0 +1,74 @@
+// @ts-nocheck
+
+import { InvalidEventParamsError } from "../../../src/common/errors"
+import { handler } from "../../../src/functions/saveEncompassLoan"
+
+describe('saveEncompassLoan', () => {
+    test('throws an InvalidEventParamsError if loan.id is missing on event detail', async () => {
+        const invalidHandler = handler({}, {}, () => {})
+
+        await expect(invalidHandler).rejects.toThrow(InvalidEventParamsError);
+        await expect(invalidHandler).rejects.toThrow('Required parameter detail.loan.id is missing on event payload')
+    })
+
+    test("throws an InvalidEventParamsError if loan.fields['4000'] is missing on event detail", async () => {
+        const invalidHandler = handler({
+            detail: {
+                loan: {
+                    id: '1'
+                }
+            }
+        }, {}, () => {})
+
+        await expect(invalidHandler).rejects.toThrow(InvalidEventParamsError);
+        await expect(invalidHandler).rejects.toThrow("Required parameter detail.loan.fields['4000'] is missing on event payload")
+    })
+
+    test("throws an InvalidEventParamsError if loan.fields['4002'] is missing on event detail", async () => {
+        const invalidHandler = handler({
+            detail: {
+                loan: {
+                    id: '1',
+                    fields: {
+                        '4000': 'bert'
+                    }
+                },
+            }
+        }, {}, () => {})
+
+        await expect(invalidHandler).rejects.toThrow(InvalidEventParamsError);
+        await expect(invalidHandler).rejects.toThrow("Required parameter detail.loan.fields['4002'] is missing on event payload")
+    })
+
+    test("throws an InvalidEventParamsError if loan.fields['65'] is missing on event detail", async () => {
+        const invalidHandler = handler({
+            detail: {
+                loan: {
+                    id: '1',
+                    fields: {
+                        '4000': 'bert',
+                        '4002': 'mcmuffin'
+                    }
+                },
+            }
+        }, {}, () => {})
+
+        await expect(invalidHandler).rejects.toThrow(InvalidEventParamsError);
+        await expect(invalidHandler).rejects.toThrow("Required parameter detail.loan.fields['65'] is missing on event payload")
+    })
+
+    test("it does not blow up", async () => {
+        await expect(handler({
+            detail: {
+                loan: {
+                    id: '1',
+                    fields: {
+                        '4000': 'bert',
+                        '4002': 'mcmuffin',
+                        '65': 'hfhf',
+                    }
+                },
+            }
+        }, {}, () => {})).resolves.not.toThrowError();
+    })
+})
