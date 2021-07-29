e65c26e58acc00e9a1f09adee1582c062279adaf
Jared Brown <brown.jared25@gmail.com>
2021-07-29 14:14:49 -0400

    Removing records from the queue after processing

--- src/functions/eFolderUDNReportUploadReplay.ts
+++ src/functions/eFolderUDNReportUploadReplay.ts
@@ -1,4 +1,5 @@
 import { SQSHandler, SQSEvent, SQSRecord } from 'aws-lambda';
+import { SQSClient, DeleteMessageCommand } from '@aws-sdk/client-sqs';
 import { EventBridgeClient, PutEventsCommand, PutEventsCommandOutput, PutEventsRequestEntry } from '@aws-sdk/client-eventbridge';
 import get from 'lodash/get';
 
@@ -16,7 +17,11 @@ export const handler: SQSHandler = async (event: SQSEvent): Promise<any> => {
 
     const records = get(event, 'Records') ?? [];
 
-    const entries: Array<PutEventsRequestEntry> = records.map((record: SQSRecord): PutEventsRequestEntry => {
+    const eventBridgeClient = new EventBridgeClient({
+        region,
+    }); 
+
+    records.forEach(async (record: SQSRecord): Promise<void> => {
         const body = get(record, 'body') ?? '{}';
         const event = JSON.parse(body);
 
@@ -32,21 +37,47 @@ export const handler: SQSHandler = async (event: SQSEvent): Promise<any> => {
         // Grab it, incrememnt it
         const retries = (get(detail, 'retries') ?? 0) + 1;
 
-        return {
-            Detail: JSON.stringify(Object.assign({}, detail, {
-                retries,
-            })),
-            DetailType: detailType,
-            EventBusName: eventBus,
-            Source: source
-        }
-    });
+        await eventBridgeClient.send(new PutEventsCommand({
+            Entries: [{
+                Detail: JSON.stringify(Object.assign({}, detail, {
+                    retries,
+                })),
+                DetailType: detailType,
+                EventBusName: eventBus,
+                Source: source
+            }],
+        }))
 
-    const client = new EventBridgeClient({
+        const receiptHandler = get(record, 'receiptHandle');
+        // This should never happen, but if it does, we move on to the next record — the message will get removed from the queue upon
+        // successful completion of the lambda
+        if (!receiptHandler) 
+            return;
+        // If any record in a batch fails, all messages in that batch remain on the queue, even if they were already
+        // processed. This line removes successfully processed records from the queue so they're not re-processed in
+        // the event another record in the batch fails.
+        await removeFromQueue(receiptHandler);
+    });
+};
+/**
+ * Clear the message from the queue
+ * @param {string} receiptHandler 
+ */
+ const removeFromQueue = async (receiptHandler: string): Promise<void> => {
+    const region = get(process, 'env.REGION');
+    if (!region) throw new Error('Environment missing REGION variable');
+
+    const accountId = get(process, 'env.ACCOUNT_ID');
+    if (!accountId) throw new Error('Environment missing ACCOUNT_ID variable');
+
+    const queueName = get(process, 'env.QUEUE_NAME');
+    if (!queueName) throw new Error('Environment missing QUEUE_NAME variable');
+
+    const sqs = new SQSClient({
         region,
     });
-
-    await client.send(new PutEventsCommand({
-        Entries: entries,
-    }))
+    await sqs.send(new DeleteMessageCommand({
+        ReceiptHandle: receiptHandler,
+        QueueUrl: `https://sqs.${region}.amazonaws.com/${accountId}/${queueName}`,
+    }));
 };
