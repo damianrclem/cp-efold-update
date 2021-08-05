a90ad169fce5b15ae82aa38a5b64deff1a17444a
Mike Ryan <mike@mikeryan.me>
2021-08-05 09:36:32 -0700

    DZ-484 adding in new interface value for prod

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,12 +1,6 @@
 export const UDN_REPORTS_E_FOLDER_DOCUMENT_TITLE = 'Credit - LQCC';
 export const UDN_REPORTS_E_FOLDER_DOCUMENT_DESCRIPTION = 'Credit update - Softpull or UDN report';
 export const RM_CLIENT = 'cp-efolder-upload';
-export const STAGE = {
-    PR: 'pr',
-    DEV: 'dev',
-    STAGE: 'stage',
-    PROD: 'prod'
-};
 
 export const AUDIT_FIELDS: Array<string> = [
     "CX.CTC.AUDIT1",
