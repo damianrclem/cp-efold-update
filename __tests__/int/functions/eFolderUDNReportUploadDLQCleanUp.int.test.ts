f95e6572476a8d09b640a211273c7ba3d7933da8
Jared Brown <brown.jared25@gmail.com>
2021-11-23 11:14:48 -0500

    Integration test to run deduplication

--- __tests__/int/functions/eFolderUDNReportUploadDLQCleanUp.int.test.ts
+++ __tests__/int/functions/eFolderUDNReportUploadDLQCleanUp.int.test.ts
@@ -1,9 +1,66 @@
 // @ts-nocheck
-
-import { GetQueueUrlCommand, SendMessageBatchCommand, SendMessageBatchRequestEntry, SQSClient,  } from "@aws-sdk/client-sqs";
+import { GetQueueUrlCommand, PurgeQueueCommand, SendMessageBatchCommand, SendMessageBatchRequestEntry, SQSClient } from "@aws-sdk/client-sqs";
+import { v4 as uuid } from 'uuid';
 import { handler } from "../../../src/functions/eFolderUDNReportUploadDLQCleanUp";
 
 let OLD_ENV;
+
+const randomNumberFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max -  min + 1) - min);
+}
+
+const createTestMessages = async (numberOfUniqueMessages: number, numberOfDuplicates: number) => {
+    const messages: SendMessageBatchRequestEntry[] = [];
+
+    for (let index = 0; index <= numberOfUniqueMessages; index++) {
+        const loanId = `${Date.now()}`
+
+        for (let index = 0; index <= numberOfDuplicates; index++) {
+            messages.push({
+                Id: uuid(),
+                MessageBody: JSON.stringify({
+                    time: new Date().toString(),
+                    detail: {
+                        loan: {
+                            id: loanId
+                        }
+                    }
+                })
+            })
+        }
+    }
+
+    const sqsClient = new SQSClient({
+        region: 'us-east-2'
+    });
+
+    const { QueueUrl } = await sqsClient.send(new GetQueueUrlCommand({
+        QueueName: process.env.QUEUE_NAME
+    }));
+
+    for (let index = 0; index < messages.length; index += 10) {
+        const messageBatch = messages.slice(index, index + 10);
+
+        await sqsClient.send(new SendMessageBatchCommand({
+            QueueUrl,
+            Entries: messageBatch
+        }))
+    }
+};
+
+const purgeQueueOfTestMessages = async () => {
+    const sqsClient = new SQSClient({
+        region: 'us-east-2'
+    });
+
+    const { QueueUrl } = await sqsClient.send(new GetQueueUrlCommand({
+        QueueName: process.env.QUEUE_NAME
+    }));
+
+    await sqsClient.send(new PurgeQueueCommand({
+        QueueUrl
+    }));
+}
 
 describe('eFolderUDNReportUploadDLQCleanUp', () => {
     beforeAll(() => {
@@ -20,32 +77,13 @@ describe('eFolderUDNReportUploadDLQCleanUp', () => {
     })
 
     test('works', async () => {
-        const messages: SendMessageBatchRequestEntry[] = [
-            {
-                Id: "1",
-                MessageBody: JSON.stringify({
-                    detail: {
-                        loan: {
-                            id: "123"
-                        }
-                    }
-                })
-            }
-        ];
+        const numberOfUniqueMessages = randomNumberFromInterval(1, 5);
+        const numberOfDuplicates = randomNumberFromInterval(1, 10);
 
-        const sqsClient = new SQSClient({
-            region: 'us-east-2'
-        });
+        await createTestMessages(numberOfUniqueMessages, numberOfDuplicates);
 
-        const { QueueUrl } = await sqsClient.send(new GetQueueUrlCommand({
-            QueueName: 'cp-efolder-upload-dev-eFolderUDNReportUpload-dlq'
-        }));
+        await expect(handler({}, {}, () => {})).resolves.not.toThrow();
 
-        await sqsClient.send(new SendMessageBatchCommand({
-            QueueUrl,
-            Entries: messages
-        }))
-
-        await handler({}, {}, () => {})
-    })
+        await purgeQueueOfTestMessages();
+    });
 })
